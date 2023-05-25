7299774976e37a13e9e2c538dac67aa529b940ad
mzz <2831376148@qq.com>
2023-05-25 15:15:06 +0800

    upload

---
+++ musicobj02/src/components/SwiperCom.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        我是swipercom
+    </div>
+</template>
+
+<script>
+export default {
+    name: "swipercom"
+}
+</script>
---
+++ musicobj02/src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+      <!-- 头部导航 -->
+      <top-nav></top-nav>
+      <!-- 轮播图 -->
+      <swiper-com></swiper-com>
+      <!-- icon列表 -->
+      <icon-list></icon-list>
+      <!-- 音乐列表 -->
+      <music-list></music-list>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import TopNav from '@/components/TopNav.vue'
+import IconList from '@/components/IconList.vue'
+import SwiperCom from '@/components/SwiperCom.vue'
+import MusicList from '@/components/MusicList.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    TopNav,
+    IconList,
+    SwiperCom,
+    MusicList
+  }
+}
+</script>
+
+<style>
+
+</style>
+
